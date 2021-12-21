0a82613f3268a94a414ae23b3546d797d5407397
Marcin Skowronek <marcinfoto35@op.pl>
2021-12-21 23:21:05 +0100

    Deploy try on firebase

---
+++ build/precache-manifest.55db7fb484bb36c91a75673a522969f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97a5dca097f70b3d98b20102216f746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a10aba3b66498c4d397",
+    "url": "/static/css/main.70c13f6c.chunk.css"
+  },
+  {
+    "revision": "122e77a93b030767ac17",
+    "url": "/static/js/2.58c38005.chunk.js"
+  },
+  {
+    "revision": "5a10aba3b66498c4d397",
+    "url": "/static/js/main.b78b5346.chunk.js"
+  },
+  {
+    "revision": "e5fe30c8e6f0c4d62153",
+    "url": "/static/js/runtime-main.bd803103.js"
+  },
+  {
+    "revision": "915a06d5b1edfec91970ea68dc194644",
+    "url": "/static/media/chain.915a06d5.png"
+  }
+]);
